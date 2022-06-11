041a5122cdb3217a7136b3eca426d742ac1c374a
Fernando Cejas <android10@fernandocejas.com>
2022-06-11 18:39:22 +0200

    ref: clean up and able to run a linux command.

--- automation/arch/healthy-arch.py
+++ automation/arch/healthy-arch.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python3
 
+from subprocess import Popen, PIPE
+
 class Utility:
-    def __init__(self, description, command):
+    def __init__(self, description, commands):
         self.description = description
+        self.commands = commands
+
+    # def execute(self):
+        # run_command(self.command)
+
+class Command:
+    def __init__(self, command):
         self.command = command
 
     def run(self):
-        return self.command
+        try:
+            process = Popen(command, text=True, stdout=PIPE, stderr=PIPE)
+            out, err = process.communicate()
+            print(out)
+        except:
+            print("There was an error, please try again.")
 
 class Menu:
     def __init__(self, items):
         self.items = items
 
     def show(self):
-        print('TODO MENU: make it nice and colourful')
-        print('SELECT AN OPTION: ')
+        # PRINT ALL THE ITEMS
+        option = int(input("Please select an OPTION: "))
+
 
 def main():
-    menu = Menu([
-        Utility('0 - System Information.', 'neofetch'), 
-        Utility('1 - System Upgrade.', 'sudo pacman -Syu')]
-        ).show()
+    Menu([
+        Utility('0 - System Information.', ["neofetch"]), 
+        Utility('1 - System Upgrade.', ["sudo pacman -Syu"]),
+        Utility('2 - Exit.', ["exit"])
+        ]).show()
 
 # Start the script
-main()
+while (True):
+    main()
