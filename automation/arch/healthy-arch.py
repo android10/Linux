dd68ae25044ad2634bb2e6f37c1529c52cc47261
Fernando Cejas <android10@fernandocejas.com>
2022-06-11 20:41:46 +0200

    ref: improve code readability for the arch healthy script.

--- automation/arch/healthy-arch.py
+++ automation/arch/healthy-arch.py
@@ -1,41 +1,64 @@
 #!/usr/bin/env python3
 
+import shutil
 from subprocess import Popen, PIPE
 
 class Utility:
-    def __init__(self, description, commands):
-        self.description = description
-        self.commands = commands
+    def __init__(self, description, command):
+        self._description = description
+        self._command = command
 
-    # def execute(self):
-        # run_command(self.command)
+    @property
+    def description(self):
+        return self._description
+
+    def execute(self):
+        Command(self._command).run()
 
 class Command:
     def __init__(self, command):
-        self.command = command
+        self._command = shutil.which(command)
 
     def run(self):
         try:
-            process = Popen(command, text=True, stdout=PIPE, stderr=PIPE)
+            # https://docs.python.org/3/library/subprocess.html
+            process = Popen(self._command, text=True, stdout=PIPE, stderr=PIPE)
             out, err = process.communicate()
             print(out)
         except:
-            print("There was an error, please try again.")
+            print("There was an error!!! Please try again ;).")
 
 class Menu:
     def __init__(self, items):
-        self.items = items
+        self._items = items
 
     def show(self):
-        # PRINT ALL THE ITEMS
-        option = int(input("Please select an OPTION: "))
+        self.print_header()
+        self.print_items()
+        self.print_footer()
+        self.wait_for_option()
 
+    def print_header(self):
+        print('----------------------------')
+        print('  Healthy Arch Linux 1.0.0  ')
+        print('----------------------------')
+
+    def print_items(self):
+        for item in self._items:
+            print(item.description)
+
+    def print_footer(self):
+        print('----------------------------')
+
+    def wait_for_option(self):
+        option = int(input("Please select an OPTION: "))
+        self._items[option].execute()
 
 def main():
     Menu([
-        Utility('0 - System Information.', ["neofetch"]), 
-        Utility('1 - System Upgrade.', ["sudo pacman -Syu"]),
-        Utility('2 - Exit.', ["exit"])
+        Utility('0 - System Information.', "neofetch"), 
+        Utility('1 - System Upgrade.', "sudo pacman -Syu"),
+        Utility('2 - Exit.', "exit 0")
         ]).show()
 
 # Start the script
